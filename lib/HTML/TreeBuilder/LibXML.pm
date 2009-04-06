7c6be553cbb80f4fc23b3fd7340a60dedeb6d2c9
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-04-06 11:10:34 +0900

    Checking in changes prior to tagging of version 0.04.  Changelog diff is:
    diff --git a/Changes b/Changes
    index 10dd253..4e55988 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,13 @@
     Revision history for Perl extension HTML::TreeBuilder::LibXML
    
    +0.04
    +
    +        - support more look_down() params(miyagawa)
    +        - added ->id support(tokuhirom)
    +        - call ->eof automatically for compatibility.(tokuhirom)
    +        - added ->findvalue support(tokuhirom)
    +        - added ->eof(tokuhirom)
    +
     0.03
    
             - [FEATURE] added to restore hacked constructor(suggested by miyagawa++).

--- lib/HTML/TreeBuilder/LibXML.pm
+++ lib/HTML/TreeBuilder/LibXML.pm
@@ -1,7 +1,7 @@
 package HTML::TreeBuilder::LibXML;
 use strict;
 use warnings;
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 use Carp ();
 use base 'HTML::TreeBuilder::LibXML::Node';
 use XML::LibXML;
