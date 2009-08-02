938af1b70593da86d0ff3919d3aa083b3c8987d5
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2009-08-02 06:03:27 -0700

    Checking in changes prior to tagging of version 0.09.  Changelog diff is:
    diff --git a/Changes b/Changes
    index 071df5a..c65dfe9 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,6 +1,10 @@
     Revision history for Perl extension HTML::TreeBuilder::LibXML
    
    -0.08  Mon Jul 20 10:00:19 PDT 2009
    +0.09 Sun Aug  2 06:02:01 PDT 2009
    +     - Fixed parse_html method when parsing whitespace strings so it won't break and consistent with HTML::TB
    +       (Reported by otsune)
    +
    +0.08 Mon Jul 20 10:00:19 PDT 2009
             - Updated POD document
    
     0.07

--- lib/HTML/TreeBuilder/LibXML.pm
+++ lib/HTML/TreeBuilder/LibXML.pm
@@ -1,7 +1,7 @@
 package HTML::TreeBuilder::LibXML;
 use strict;
 use warnings;
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 use Carp ();
 use base 'HTML::TreeBuilder::LibXML::Node';
 use XML::LibXML;
