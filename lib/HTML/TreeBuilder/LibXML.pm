a8b8356313eb44ca87bea39f956b1aed9d124241
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2009-08-02 06:01:49 -0700

    make parse_html_* not break when parsing empty strings.

--- lib/HTML/TreeBuilder/LibXML.pm
+++ lib/HTML/TreeBuilder/LibXML.pm
@@ -50,13 +50,22 @@ sub parse {
 sub parse_file {
     my $self = shift;
     my $doc  = $self->_parser->parse_html_file(@_);
-    $self->{node} = $doc->documentElement;
+    $self->{node} = $self->_documentElement($doc);
 }
 
 sub eof {
     my ($self, ) = @_;
     my $doc = $self->_parser->parse_html_string($self->{_content});
-    $self->{node} = $doc->documentElement;
+    $self->{node} = $self->_documentElement($doc);
+}
+
+sub _documentElement {
+    my($self, $doc) = @_;
+    return $doc->documentElement || do {
+        my $elem = $doc->createElement("html");
+        $elem->appendChild($doc->createElement("body"));
+        $elem;
+    };
 }
 
 sub replace_original {
---
+++ t/05_empty.t
@@ -0,0 +1,18 @@
+use strict;
+use Test::Base;
+use HTML::TreeBuilder::LibXML;
+
+my @tests = ("foo", "bar\nbaz", "   ", "   \n   ");
+plan tests => 3 * @tests;
+
+for my $t (@tests) {
+    my $tree = HTML::TreeBuilder::LibXML->new;
+    $tree->parse($t);
+    $tree->eof;
+
+    ok $tree;
+    eval { $tree->findnodes('//a') };
+    ok !$@ or diag $@;
+
+    like $tree->as_HTML, qr/<html>/;
+}
