aa98874809c1cf513e07c8d10659ea1c036d75b5
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2009-10-06 10:48:27 -0700

    Checking in changes prior to tagging of version 0.11.  Changelog diff is:
    diff --git a/Changes b/Changes
    index 4b18b92..f127b02 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,7 +1,10 @@
     Revision history for Perl extension HTML::TreeBuilder::LibXML
    
    +0.11 Tue Oct  6 10:47:16 PDT 2009
    +     - Fix parse when a content is truly an empty string
    +
     0.10
    -    - added as_trimmed_text(chiba)
    +     - added as_trimmed_text(chiba)
    
     0.09 Sun Aug  2 06:02:01 PDT 2009
          - Fixed parse_html method when parsing whitespace strings so it won't break and consistent with HTML::TB

--- lib/HTML/TreeBuilder/LibXML.pm
+++ lib/HTML/TreeBuilder/LibXML.pm
@@ -1,7 +1,7 @@
 package HTML::TreeBuilder::LibXML;
 use strict;
 use warnings;
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 use Carp ();
 use base 'HTML::TreeBuilder::LibXML::Node';
 use XML::LibXML;
