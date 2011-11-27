2256a70fbe57cc476281095dd21db5e133ef9af1
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-11-27 16:29:14 +0900

    Checking in changes prior to tagging of version 0.14.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index a43a0c2..0734fd8 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,10 @@
     Revision history for Perl extension HTML::TreeBuilder::LibXML
    
    +0.14
    +
    +    * added workaround for Web::Scraper 0.36
    +      (tokuhirom)
    +
     0.13
          * Added 'getValue' node's method as in HTML::TreeBuilder::XPath for
            comment nodes in web-scraper module

--- lib/HTML/TreeBuilder/LibXML.pm
+++ lib/HTML/TreeBuilder/LibXML.pm
@@ -1,7 +1,7 @@
 package HTML::TreeBuilder::LibXML;
 use strict;
 use warnings;
-our $VERSION = '0.13';
+our $VERSION = '0.14';
 use Carp ();
 use base 'HTML::TreeBuilder::LibXML::Node';
 use XML::LibXML;
