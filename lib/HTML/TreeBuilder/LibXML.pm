f28b41304dc213ee9c1605cb4fc64a5eae25fa02
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-11-30 03:21:44 +0900

    Checking in changes prior to tagging of version 0.15.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 2f28a14..4d4994d 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,6 +1,6 @@
     Revision history for Perl extension HTML::TreeBuilder::LibXML
    
    -{{NEXT}}
    +0.15
    
         * Add additional methods to better match
           HTML::TreeBuilder::XPath::Node API:

--- lib/HTML/TreeBuilder/LibXML.pm
+++ lib/HTML/TreeBuilder/LibXML.pm
@@ -1,7 +1,7 @@
 package HTML::TreeBuilder::LibXML;
 use strict;
 use warnings;
-our $VERSION = '0.14';
+our $VERSION = '0.15';
 use Carp ();
 use base 'HTML::TreeBuilder::LibXML::Node';
 use XML::LibXML;
