a0b446859658c7776f3f402e91a701977e1b985c
Tokuhiro Matsuno <tokuhirom@gmail.com>
2012-04-02 21:30:13 +0900

    Checking in changes prior to tagging of version 0.16.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 4d4994d..62e61e7 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,14 @@
     Revision history for Perl extension HTML::TreeBuilder::LibXML
    
    +0.16
    +
    +
    +    commit 07b40205fd03564d476eff7675e9f19196939f2f
    +    Author: Oleg G <verdrehung@gmail.com>
    +    Date:   Sat Mar 31 13:26:11 2012 +0700
    +
    +    added few methods to support Web::Query
    +
     0.15
    
         * Add additional methods to better match

--- lib/HTML/TreeBuilder/LibXML.pm
+++ lib/HTML/TreeBuilder/LibXML.pm
@@ -1,7 +1,7 @@
 package HTML::TreeBuilder::LibXML;
 use strict;
 use warnings;
-our $VERSION = '0.15';
+our $VERSION = '0.16';
 use Carp ();
 use base 'HTML::TreeBuilder::LibXML::Node';
 use XML::LibXML;
