8414d22990b93f2fa030d58caf0f5f471b5dfaba
Carlos Fernando Avila Gratz <cafe@q1software.com>
2013-05-09 22:04:05 -0300

    Checking in changes prior to tagging of version 0.19.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 6e16577..9f40f79 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,12 @@ Revision history for Perl extension HTML::TreeBuilder::LibXML
    
     {{$NEXT}}
    
    +0.19 2013-05-10T01:03:58Z
    +
    +    - fixed replace_with() and parent(),
    +      to avoid calling appendChild() on a Document node, which is not supported by XML::LibXML.
    +      (cafe01)
    +
     0.18 2013-05-09T20:49:04Z
    
         - implemented all node methods needed for Web::Query::LibXML to work

--- lib/HTML/TreeBuilder/LibXML.pm
+++ lib/HTML/TreeBuilder/LibXML.pm
@@ -1,7 +1,7 @@
 package HTML::TreeBuilder::LibXML;
 use strict;
 use warnings;
-our $VERSION = '0.18';
+our $VERSION = '0.19';
 use Carp ();
 use base 'HTML::TreeBuilder::LibXML::Node';
 use XML::LibXML;
