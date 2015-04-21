aa9c1770870402975ecee89d49270b1651d28fa3
Tokuhiro Matsuno <tokuhirom@gmail.com>
2015-04-22 08:39:46 +0900

    Checking in changes prior to tagging of version 0.25.
    Changelog diff is:
    
    diff --git Changes Changes
    index 1aa3c7f..03ce7a3 100644
    --- Changes
    +++ Changes
    @@ -2,6 +2,18 @@ Revision history for Perl extension HTML::TreeBuilder::LibXML
    
     {{$NEXT}}
    
    +0.25 2015-04-21T23:39:33Z
    +
    +    commit 5c46775d50678e611729135f3601b66e6a0d10cd
    +    Author: Yanick Champoux <yanick@babyl.dyndns.org>
    +    Date:   Thu Jan 15 14:15:04 2015 -0500
    +
    +        silence warnings
    +
    +        ... as the arguments can be undefined
    +
    +        Fixes #10
    +
     0.24 2014-09-22T09:31:26Z
    
         - Removed Web::Scraper from runtime dependencies.

--- lib/HTML/TreeBuilder/LibXML.pm
+++ lib/HTML/TreeBuilder/LibXML.pm
@@ -1,7 +1,7 @@
 package HTML::TreeBuilder::LibXML;
 use strict;
 use warnings;
-our $VERSION = '0.24';
+our $VERSION = '0.25';
 use Carp ();
 use base 'HTML::TreeBuilder::LibXML::Node';
 use XML::LibXML;
